97ae8551173fe05126c2bdd93ad37b0d7089d896
= <=>
2020-04-08 10:37:16 +0100

    Install Laravel and set up Boostrap template

---
+++ resources/views/layouts/topnav.blade.php
@@ -0,0 +1,8 @@
+<nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+	<a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">CampaignTools</a>
+	<ul class="navbar-nav px-3">
+		<li class="nav-item ">
+			<a class="nav-link" href="#">Profile</a>
+		</li>
+	</ul>
+</nav>
